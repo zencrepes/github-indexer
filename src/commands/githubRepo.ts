dab166205feadcbc22ad004346ec7c2953c04e54
fgerthoffert <f.gerthoffert@gmail.com>
2019-05-27 15:31:51 -0400

    First run on a working repo fetching system

--- src/commands/githubRepo.ts
+++ src/commands/githubRepo.ts
@@ -1,7 +1,10 @@
-//import {ApiResponse, Client} from '@elastic/elasticsearch'
+import {ApiResponse, Client} from '@elastic/elasticsearch'
 import {Command, flags} from '@oclif/command'
 import cli from 'cli-ux'
+import * as fs from 'fs'
+import * as jsYaml from 'js-yaml'
 import * as loadYamlFile from 'load-yaml-file'
+import * as _ from 'lodash'
 import * as path from 'path'
 
 import FetchAffiliated from '../utils/github/fetchAffiliated/index'
@@ -43,14 +46,15 @@ export default class GithubRepo extends Command {
     const {flags} = this.parse(GithubRepo)
     const {grab, org, repo} = flags
     const userConfig = await loadYamlFile(path.join(this.config.configDir, 'config.yml'))
+    const es_port = userConfig.elasticsearch.port
+    const es_host = userConfig.elasticsearch.host
 
+    //1- Grab the repositories from GitHub
     let fetchedRepos = []
     if (grab === 'affiliated') {
       this.log('Starting to fetch data from affiliated organizations')
       const fetchData = new FetchAffiliated(this.log, this.error, userConfig, cli)
       fetchedRepos = await fetchData.load()
-      //Return all repos as a big array
-
     } else if (grab === 'org' && org !== undefined) {
       this.log('Starting to fetch data from org: ' + org)
       const fetchData = new FetchOrg(this.log, this.error, userConfig, cli)
@@ -60,52 +64,121 @@ export default class GithubRepo extends Command {
       const fetchData = new FetchRepo(this.log, this.error, userConfig, cli)
       fetchedRepos = await fetchData.load(org, repo)
     }
-    this.log(fetchedRepos)
-  }
-/*
-    // Force the user either to manually press y or to specify the force flag in the command line
-    let proceed = true
-    if (!force) {
-      const userForce = await cli.prompt('Are you sure you want to push a new index ? It will ERASE your data (y/n)')
-      proceed = (userForce === 'y')
-    } else {
-      proceed = true
+    const reposIndexName = 'gh_repos'
+
+    //2- Test if an index exists, if it does not, create it.
+    cli.action.start('Checking if repository: ' + reposIndexName + ' exists')
+    const client = new Client({node: es_host + ':' + es_port})
+    const healthCheck: ApiResponse = await client.cluster.health()
+    if (healthCheck.body.status === 'red') {
+      this.log('Elastic search cluster is not in an healthy state, exiting')
+      this.log(healthCheck.body)
+      process.exit(1)
     }
-    if (proceed) {
-      this.log('Testing connection to the Elasticsearch cluster')
-      // tslint:disable-next-line:no-http-string
-      const client = new Client({node: 'http://' + host + ':' + port})
-      const healthCheck: ApiResponse = await client.cluster.health()
-
-      if (healthCheck.body.status === 'red') {
-        //https://nodejs.org/api/process.html#process_exit_codes
-        this.log('Elastic search cluster is not in an healthy state, exiting')
-        this.log(healthCheck.body)
-        process.exit(1)
-      }
-
-      this.log('Testing for availability of index: ' + index)
-      const testIndex = await client.indices.exists({index})
-      if (testIndex.body !== false) {
-        this.log('Index already exists, deleting')
-        await client.indices.delete({index})
-      }
-
-      this.log('Loading the mapping from file ./src/schemas/' + mapping + '.yml')
-      const mappings = await loadYamlFile('./src/schemas/' + mapping + '.yml')
-      this.log('Schema configuration loaded')
-
-      this.log('Loading the settings from file ./src/schemas/settings.yml')
+    const testIndex = await client.indices.exists({index: reposIndexName})
+    if (testIndex.body === false) {
+      cli.action.start('Repository gh_repos does not exist, creating')
+      const mappings = await loadYamlFile('./src/schemas/repositories.yml')
       const settings = await loadYamlFile('./src/schemas/settings.yml')
-      this.log(JSON.stringify(settings))
-
-      this.log('Index settings loaded')
-
-      this.log('Creating the index: ' + index)
-      await client.indices.create({index, body: {settings, mappings}})
-      this.log('Index created: ' + index)
-    } else {
-      this.log('Command cancelled')
+      await client.indices.create({index: reposIndexName, body: {settings, mappings}})
     }
-  */
+    cli.action.stop(' done')
+
+    cli.action.start('Grabbing data from ElasticSearch and merging with new data')
+    //3- Grab the repositories data from ElasticSearch
+    let esRepos = await client.search({
+      index: reposIndexName,
+      body: {
+        from: 0,
+        size: 10000,
+        query: {
+          match_all: {}
+        }
+      }
+    })
+
+    //4- Loop through the newly grabbed data and see if there is a corresponding result in ES
+    let esPayload = []
+    fetchedRepos.map((repo: object) => {
+      const existingRepo = _.find(esRepos.body.hits.hits, {id: repo.id})
+      const updatedRepo = {...repo}
+      if (existingRepo !== undefined) {
+        //If the repo exist, we are only looking for the active flag
+        updatedRepo.active = existingRepo.active
+      } else {
+        updatedRepo.active = false
+      }
+      //If submitting only one repository, assumption is that it should be active.
+      if (grab === 'repo') {
+        this.log('Activating repository: ' + repo.org.login + '/' + repo.name)
+        updatedRepo.active = true
+      }
+      esPayload.push(updatedRepo)
+    })
+    cli.action.stop(' done')
+    this.log('About to submit (create or update) data about ' + esPayload.length + ' repo(s) to Elasticsearch')
+
+    //Split the array in chunks of 100
+    const esPayloadChunked = await this.chunkArray(esPayload, 100)
+    //5- Push the results back to Elastic Search
+    for (const [idx, esPayloadChunk] of esPayloadChunked.entries()) {
+      cli.action.start('Submitting data to ElasticSearch (' + parseInt(idx + 1, 10) + ' / ' + esPayload.length + ')')
+      let formattedData = ''
+      for (let rec of esPayloadChunk) {
+        formattedData = formattedData + JSON.stringify({
+          index: {
+            _index: reposIndexName,
+            _id: rec.id
+          }
+        }) + '\n' + JSON.stringify(rec) + '\n'
+      }
+      await client.bulk({index: reposIndexName, body: formattedData})
+      cli.action.stop(' done')
+    }
+
+    //6- Update the configuration by re-downloading all data from ElasticSearch to create the configuration file
+    cli.action.start('Refreshing the repositories configuration file')
+    esRepos = await client.search({
+      index: reposIndexName,
+      body: {
+        from: 0,
+        size: 10000,
+        query: {
+          match_all: {}
+        }
+      }
+    })
+
+    this.log('')
+    this.log('All available repositories:')
+    cli.table(esRepos.body.hits.hits, {
+      name: {
+        get: row => (row._source.name + '/' + row._source.name)
+      },
+      active: {
+        get: row => row._source.active
+      },
+    }, {
+      printLine: this.log,
+    })
+    this.log('')
+    const configArray = esRepos.body.hits.hits.map(repo => {
+      return {
+        [repo._source.org.login + '/' + repo._source.name]: repo._source.active
+      }
+    })
+    fs.writeFileSync(path.join(this.config.configDir, 'repositories.yml'), jsYaml.safeDump(configArray))
+    cli.action.stop(' done')
+    this.log('You can enable/disable repositories in: ' + path.join(this.config.configDir, 'repositories.yml'))
+  }
+
+  //https://ourcodeworld.com/articles/read/278/how-to-split-an-array-into-chunks-of-the-same-size-easily-in-javascript
+  async chunkArray(srcArray, chunkSize) {
+    let idx = 0
+    let tmpArray = []
+    for (idx = 0; idx < srcArray.length; idx += chunkSize) {
+      tmpArray.push(srcArray.slice(idx, idx + chunkSize))
+    }
+    return tmpArray
+  }
 }
